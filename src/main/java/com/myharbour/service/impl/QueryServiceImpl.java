a5e4b769b42287fec69903673226bb5ffb2c65cf
XiWinnie <513768474@qq.com>
2018-05-22 16:06:51 +0800

    service层增加分页,构造测试数据

--- src/main/java/com/myharbour/service/impl/QueryServiceImpl.java
+++ src/main/java/com/myharbour/service/impl/QueryServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,7 +17,20 @@ public class QueryServiceImpl implements QueryService{
     @Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.SERIALIZABLE)
     @Override
     public List<Container> getContainersBySpecificParas(Integer size, Integer type, Integer area, int page) {
-
-        return null;
+        //test
+        List<Container>containers = new ArrayList<>();
+        for(int i = 0;i< 10;i++){
+            Container container = new Container();
+            container.setContainerId(10000000+i);
+            container.setSize(i%2);
+            container.setType(i%3);
+            container.setContainerArea(i%4);
+            container.setLayer(0);
+            container.setColumn(0);
+            container.setRow(0);
+            containers.add(container);
+        }
+        return containers;
+        //test
     }
 }
